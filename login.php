4da6baf8a140e7adc37c7e3447c0ecae9325609e
Olti-s <sogojevaolti451@gmail.com>
2024-12-05 17:43:23 +0100

    ...

---
+++ login.php
@@ -0,0 +1,53 @@
+<?php include 'include/index.php'; session_start();  ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+
+  <div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card shadow">
+                <div class="card-body">
+                    <h2 class="text-center">Login in</h2>
+                    
+                    <form method="POST">
+                        <div class="mb-3">
+                         <label for="email">Email</label>
+                         <input type="email" name="email" id="email" required>
+ 
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="password">Password</label>
+                            <input type="password" name='password' id='password' class='form-control' required>
+                        </div>
+
+                        <button type='submit' class='btn btn-primary w-100' >Log in</button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
